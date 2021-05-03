d4478024f7a8322cc9c050577d06747968baeb0d
Jaired Collins <jrcqw5@mail.missouri.edu>
2021-05-03 02:53:33 -0500

    Import data, play animation, reset grid
    Big changes to show visualization of kmeans. Allows resetting and
    changing the k parameter. Adds a 'Play' button to animate the process.
    Allows speed and step to be changed. Correctly resets when desired.
    Disables certain controls while stepping.
    
    Allows import of data in specified
    file format from assignment specifications:
    
    Line 1: number of data points
    Line 2: dimension
    Line 3~end: data, space delimited

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -5,13 +5,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
                                           ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+  eMsg_ = new QErrorMessage(this);
+  eMsg_->setWindowModality(Qt::WindowModal);
 
-  ui->plot->xAxis->setRange(-10.0, 10.0);
-  ui->plot->yAxis->setRange(-10.0, 10.0);
+  kmeans_alg_ = nullptr;
+  colors_ = nullptr;
+  timer_ = new QTimer(this);
+  timer_->callOnTimeout(this, &MainWindow::Step);
 
-  QPen originPen(Qt::gray, 1);
-  ui->plot->xAxis->grid()->setZeroLinePen(originPen);
-  ui->plot->yAxis->grid()->setZeroLinePen(originPen);
+  kmeansExecuting = false;
 
   SetSignals();
 }
@@ -21,12 +23,130 @@ void MainWindow::showEvent(QShowEvent *event)
   QWidget::showEvent(event);
   ui->xMinSpinBox->selectAll();
   ui->xMinSpinBox->setFocus();
+
+  SetGridBounds(-10.0, 10.0, -10.0, 10.0);
+
+  QPen originPen(Qt::gray, 1);
+  ui->plot->xAxis->grid()->setZeroLinePen(originPen);
+  ui->plot->yAxis->grid()->setZeroLinePen(originPen);
 }
 
 void MainWindow::SetSignals()
 {
   connect(ui->createDataButton, &QPushButton::clicked,
           this, &MainWindow::GenerateData);
+  connect(ui->stepButton, &QPushButton::clicked,
+          this, &MainWindow::Step);
+  connect(ui->resetButton, &QPushButton::clicked,
+          this, &MainWindow::Reset);
+  connect(ui->playButton, &QPushButton::clicked,
+          this, &MainWindow::PlaySteps);
+  connect(ui->stopButton, &QPushButton::clicked,
+          this, &MainWindow::StopPlaying);
+  connect(ui->playSpeedSpinBox, QOverload<int>::of(&QSpinBox::valueChanged),
+          this, &MainWindow::ChangePlayTimeout);
+  connect(ui->importAction, &QAction::triggered,
+          this, &MainWindow::ImportData);
+}
+
+void MainWindow::PlaySteps()
+{
+  timer_->start(ui->playSpeedSpinBox->value());
+}
+
+void MainWindow::ChangePlayTimeout(int timeout)
+{
+  timer_->stop();
+  timer_->start(timeout);
+}
+
+void MainWindow::StopPlaying()
+{
+  timer_->stop();
+}
+
+void MainWindow::EnableControls(bool state)
+{
+  ui->nDataSpinBox->setEnabled(state);
+  ui->xMinSpinBox->setEnabled(state);
+  ui->xMaxSpinBox->setEnabled(state);
+  ui->yMinSpinBox->setEnabled(state);
+  ui->yMaxSpinBox->setEnabled(state);
+  ui->createDataButton->setEnabled(state);
+  ui->kSpinBox->setEnabled(state);
+}
+
+void MainWindow::ImportData()
+{
+  QString filename = QFileDialog::getOpenFileName(this, "Open Data File",
+                                                  "/home", tr("*.txt"));
+  qDebug() << "Filename: " << filename;
+  if (filename.isEmpty())
+    eMsg_->showMessage("Empty filename, data not set.");
+  else
+  {
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+      eMsg_->showMessage("Unable to open data file.");
+      return;
+    }
+
+    QTextStream in(&file);
+
+    QString nText = in.readLine();
+    QString dimText = in.readLine();
+
+    if (dimText.toInt() == 2) Parse2D(in);
+
+    file.close();
+    SetGridBounds(minx_, maxx_, miny_, maxy_);
+    DefaultPlot();
+    SetPairVector(xData_, yData_);
+  }
+}
+
+void MainWindow::Parse2D(QTextStream& in)
+{
+  QString line = in.readLine();
+  QStringList data = line.split(' ');
+
+  maxx_ = data[0].toDouble();
+  minx_ = data[0].toDouble();
+  xData_.append(minx_);
+
+  maxy_ = data[1].toDouble();
+  miny_ = data[1].toDouble();
+  yData_.append(miny_);
+
+  double x, y;
+  while (!in.atEnd())
+  {
+    QString line = in.readLine();
+    QStringList data = line.split(' ');
+
+    x = data[0].toDouble();
+    y = data[1].toDouble();
+    xData_.append(x);
+    yData_.append(y);
+
+    if (x > maxx_) maxx_ = x;
+    if (y > maxy_) maxy_ = y;
+    if (x < minx_) minx_ = x;
+    if (y < miny_) miny_ = y;
+  }
+}
+
+void MainWindow::DefaultPlot()
+{
+  ui->plot->clearGraphs();
+  if (ui->plot->graphCount() == 0)
+    ui->plot->addGraph();
+  QCPGraph* g = ui->plot->graph(0);
+  g->setData(xData_, yData_);
+  g->setLineStyle(QCPGraph::lsNone);
+  g->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 4));
+  ui->plot->replot();
 }
 
 void MainWindow::GenerateData()
@@ -36,21 +156,131 @@ void MainWindow::GenerateData()
   uDistd xDist(ui->xMinSpinBox->value(), ui->xMaxSpinBox->value());
   uDistd yDist(ui->yMinSpinBox->value(), ui->yMaxSpinBox->value());
 
-  xData = RandomData::Generate(xDist, gen, ui->nDataSpinBox->value());
-  yData = RandomData::Generate(yDist, gen, ui->nDataSpinBox->value());
+  xData_ = RandomData::Generate(xDist, gen, ui->nDataSpinBox->value());
+  yData_ = RandomData::Generate(yDist, gen, ui->nDataSpinBox->value());
 
-  if (ui->plot->graphCount() == 0)
-    ui->plot->addGraph();
-  QCPGraph* g = ui->plot->graph(0);
-  g->setData(xData, yData);
-  g->setLineStyle(QCPGraph::lsNone);
-  g->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 4));
-  ui->plot->replot();
+  SetPairVector(xData_, yData_);
+
+  SetGridBounds(ui->xMinSpinBox->value(), ui->xMaxSpinBox->value(),
+                ui->yMinSpinBox->value(), ui->yMaxSpinBox->value());
+
+  DefaultPlot();
+}
+
+void MainWindow::SetGridBounds(double xMin, double xMax,
+                               double yMin, double yMax)
+{
+  ui->plot->xAxis->setRange(xMin, xMax);
+  ui->plot->yAxis->setRange(yMin, yMax);
+}
+
+void MainWindow::Step()
+{
+  if (pairs_.size() == 0)
+  {
+    eMsg_->showMessage("Data not initialized. Can't perform kmeans.");
+  }
+  else
+  {
+    int k = ui->kSpinBox->value();
+    if (kmeans_alg_ == nullptr)
+      kmeans_alg_ = new kmeans<Pair2D>(k, pairs_);
+
+    if (!kmeansExecuting)
+    {
+      kmeansExecuting = true;
+      kmeans_alg_->setK(k);
+      SetColorVector(k);
+      EnableControls(false);
+    }
+
+    int stepValue = ui->stepSpinBox->value();
+    if (stepValue == 1)
+      kmeans_alg_->step(Pair2D::EuclideanDistance);
+    else
+      kmeans_alg_->step(Pair2D::EuclideanDistance, stepValue);
+    Set2DGraphData();
+  }
+}
+
+void MainWindow::Reset()
+{
+  timer_->stop();
+  DefaultPlot();
+  kmeansExecuting = false;
+  EnableControls(true);
+}
+
+void MainWindow::SetPairVector(QVector<double> x, QVector<double> y)
+{
+  pairs_.clear();
+  for (int i = 0; i < x.size(); i++)
+    pairs_.append(Pair2D(x[i], y[i]));
+}
+
+void MainWindow::Set2DGraphData()
+{
+  int k = kmeans_alg_->k();
+  QCustomPlot* plot = ui->plot;
+  QVector<Pair2D>& centroids = kmeans_alg_->centroids();
+  QVector<quint32>& assignments = kmeans_alg_->assignments();
+  PairBuckets assignedPairs = GetPairBuckets(assignments);
+
+  ui->plot->clearGraphs();
+
+  for (int i = 0; i < k; i++)
+  {
+    plot->addGraph();
+    QCPGraph* g = plot->graph(i);
+    g->setData(assignedPairs[i].first, assignedPairs[i].second);
+    g->setLineStyle(QCPGraph::lsNone);
+    g->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 4));
+    g->setPen(QPen(colors_->at(i)));
+  }
+
+  for (int i = 0; i < k; i++)
+  {
+    plot->addGraph();
+    QCPGraph* g = plot->graph(i + k);
+    QVector<double> x, y;
+    x.append(centroids[i][0]);
+    y.append(centroids[i][1]);
+    g->setData(x, y);
+    g->setLineStyle(QCPGraph::lsNone);
+    g->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCrossCircle, 20));
+    g->setPen(QPen(colors_->at(i)));
+  }
+  plot->replot();
+}
+
+void MainWindow::SetColorVector(int k)
+{
+  colors_ = new QVector<QColor>();
+
+  for (int i = 0; i < k; i++)
+    colors_->append(QColor::fromHsvF(fmod(0.618033988749895 * i, 1.0),
+                                     1.0, 1.0));
+}
+
+PairBuckets MainWindow::GetPairBuckets(QVector<quint32> &assignments)
+{
+  PairBuckets assignedPairs(kmeans_alg_->k());
+
+  for (int i = 0; i < assignments.size(); i++)
+  {
+    assignedPairs[assignments[i]].first.append(xData_[i]);
+    assignedPairs[assignments[i]].second.append(yData_[i]);
+  }
+
+  return assignedPairs;
 }
 
 MainWindow::~MainWindow()
 {
   delete ui;
-  delete rndG;
+  delete rndG_;
+  delete eMsg_;
+  delete kmeans_alg_;
+  delete colors_;
 }
 
--- MainWindow.h
+++ MainWindow.h
@@ -3,15 +3,66 @@
 
 #include <QMainWindow>
 #include <QVector>
+#include <QPair>
 #include <RandomData.h>
 #include <kmeans.h>
 #include <random>
+#include <iostream>
+#include <QDebug>
+#include <QErrorMessage>
+#include <QtMath>
+#include <QGradient>
+#include <QPen>
+#include <QTimer>
+#include <QtGlobal>
+#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 typedef std::uniform_real_distribution<double> uDistd;
+typedef QPair<double, double> QPair_d;
+typedef QVector<QPair<QVector<double>, QVector<double>>> PairBuckets;
+
+struct Pair2D
+{
+  QPair_d pair_;
+
+  Pair2D() {}
+  Pair2D(double x, double y) { pair_ = QPair_d(x, y); }
+
+  double operator[](const bool& i) const
+  {
+    // false == 0, true == 1
+    if (i) return pair_.second;
+    else return pair_.first;
+  }
+
+  Pair2D operator+(const Pair2D& rhs)
+  {
+    Pair2D sum(pair_.first + rhs[0], pair_.second + rhs[1]);
+    return sum;
+  }
+
+  Pair2D& operator+=(const Pair2D& rhs)
+  {
+    pair_.first += rhs[0];
+    pair_.second += rhs[1];
+    return *this;
+  }
+
+  Pair2D operator/(const quint32& scalar)
+  {
+    Pair2D quotient(pair_.first / scalar, pair_.second / scalar);
+    return quotient;
+  }
+
+  static double EuclideanDistance(Pair2D lhs, Pair2D rhs)
+  {
+    return qSqrt(qPow(lhs[0] - rhs[0], 2) + qPow(lhs[1] - rhs[1], 2));
+  }
+};
 
 class MainWindow : public QMainWindow
 {
@@ -23,18 +74,40 @@ public:
 
   void SetSignals();
   void GenerateData();
+  void SetGridBounds(double xMin, double xMax, double yMin, double yMax);
+  void Step();
+  void Reset();
+  void SetPairVector(QVector<double> x, QVector<double> y);
+  void Set2DGraphData();
+  void SetColorVector(int k);
+  void PlaySteps();
+  void StopPlaying();
+  void EnableControls(bool state);
+  void ImportData();
+  void Parse2D(QTextStream& in);
+  void DefaultPlot();
+  PairBuckets GetPairBuckets(QVector<quint32>& assignments);
 
-  QVector<double> xData;
-  QVector<double> yData;
+  QVector<double> xData_;
+  QVector<double> yData_;
+  QVector<Pair2D> pairs_;
+  double minx_, miny_, maxx_, maxy_;
 
 protected:
   void showEvent(QShowEvent* event);
 
 public slots:
-
+  void ChangePlayTimeout(int timeout);
+//  void ChangeK(int k);
 
 private:
+  bool kmeansExecuting = false;
+
   Ui::MainWindow *ui;
-  RandomData* rndG;
+  RandomData* rndG_;
+  QErrorMessage* eMsg_;
+  kmeans<Pair2D>* kmeans_alg_;
+  QVector<QColor>* colors_;
+  QTimer* timer_;
 };
 #endif // MAINWINDOW_H
